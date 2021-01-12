743a2cf9393e1894731cbe5f373c90126f22aff6
urunov <urunovfirdavsjon@gmail.com>
2021-01-12 23:45:51 +0500

    new feature/filter (beta)

--- src/components/layouts/TodoList/TodoList.js
+++ src/components/layouts/TodoList/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, shallowEqual } from 'react-redux'
 import TodoItem  from '../TodoItem/TodoItem'
 
@@ -6,23 +6,46 @@ import "./TodoList.css";
 
 export default function TodoList() {
     const todos = useSelector(state => state.todos, shallowEqual);
-    
+    const [tods, setTods] = useState(todos);
+
     // сортировка по приоритету
-    todos.sort((a, b) => {
+    tods.sort((a, b) => {
         if (+a.priority < +b.priority) return 1;
         if (+a.priority > +b.priority) return -1;
         return 0;
     });
 
+    const handleChangeFilter = evt => {
+        const { value } = evt.target;
+
+        switch(value) {
+            case "done":
+                return setTods(todos.filter(t => t.done === true));
+            case "notDone":
+                return setTods(todos.filter(t => t.done === false));
+            default:
+                return setTods(todos);
+        }
+    };
+    useEffect(() => {
+        setTods(todos)
+    }, [todos])
+
+
     return (
-        <>
         <div className="TodoList">
+            <div className="todos__header">
+                <select onChange={handleChangeFilter}>
+                    <option value="all">Все</option>
+                    <option value="done">Сделанные</option>
+                    <option value="notDone">Не сделанные</option>
+                </select>
+            </div>
             <div className="todos__content">
                 <ul>
-                   {todos.map((t, i) => <TodoItem key={t.id} todo={t} index={i} />)} 
+                   {tods.map((t, i) => <TodoItem key={t.id} todo={t} index={i} />)} 
                 </ul>
             </div>
         </div>
-        </>
     )
 }
